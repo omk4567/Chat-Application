14f7669dc4e8afb9bad798e35aa26aa9d8de4c46
Omkar <omkaraher2002@gmail.com>
2024-12-21 21:49:05 +0530

    Initial client side setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react'; 
+import {StreamChat} from 'stream-chat'; 
+import {Chat} from 'stream-chat-react'; 
+import Cookies from 'universal-cookie';   
+
+import { ChannelContainer, ChannelListContainer } from './components';   
+
+import './App.css'; 
+
+const apiKey = "jbmv2zh6rpf6"; 
+
+const client = StreamChat.getInstance(apiKey); 
 
 const App = () => {
   return (
-    <div>
-        <h1> Medical Pager Chat Application </h1> 
+    <div className="app__wrapper">
+        <Chat client={client} theme="team light">
+            <ChannelListContainer
+            
+            /> 
+            <ChannelContainer
+            
+            />
+        </Chat>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
-import react from 'react'; 
-import ReactDOM from 'react-dom'; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import App from './App'; 
+// Create a root container
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById('root')); 
+// Render the App component
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); 
