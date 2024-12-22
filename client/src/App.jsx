97b3d7314706f834fa42486f7c0cfae8148d0987
Omkar <omkaraher2002@gmail.com>
2024-12-22 16:18:25 +0530

    frontend signup and signin form done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,18 @@ import {StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react'; 
 import Cookies from 'universal-cookie';   
 
-import { ChannelContainer, ChannelListContainer } from './components';   
+import { ChannelContainer, ChannelListContainer, Auth } from './components';   
 
 import './App.css'; 
 
 const apiKey = "jbmv2zh6rpf6"; 
 
-const client = StreamChat.getInstance(apiKey); 
+const client = StreamChat.getInstance(apiKey);  
 
-const App = () => {
+const authToken = false; 
+
+const App = () => { 
+  if(!authToken) return <Auth /> 
   return (
     <div className="app__wrapper">
         <Chat client={client} theme="team light">
